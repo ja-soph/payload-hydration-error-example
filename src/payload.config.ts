4fffd01fec40a720f3eab48d8939a4e339c3480f
sophie_j <sophie.j@arkulpa.at>
2024-12-18 16:40:07 +0100

    adjust files for reproduction

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -8,7 +8,8 @@ import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
 import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+
+import Bookings from './collections/BookingsExample'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -20,7 +21,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Bookings],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
